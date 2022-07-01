af5eb600187d30df2136b49d0f9b030a282eb3c5
bborabora-ko <bborabora.ko@gmail.com>
2022-07-01 21:50:18 +0900

    making bbs(a post)

--- BBS/src/main/java/bbs/BbsDAO.java
+++ BBS/src/main/java/bbs/BbsDAO.java
@@ -112,5 +112,26 @@ public class BbsDAO {
 		return false; // 다음페이지 필요없음(데이터 없음)
 	}
 	
-	
+	//게시글 한개 보기
+	public Bbs getBbs(int bbsID) {
+		String SQL = "SELECT * FROM BBS WHERE bbsID = ?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, bbsID);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				Bbs bbs = new Bbs();
+				bbs.setBbsID(rs.getInt(1));
+				bbs.setBbsTitle(rs.getString(2));
+				bbs.setUserID(rs.getString(3));
+				bbs.setBbsDate(rs.getString(4));
+				bbs.setBbsContent(rs.getString(5));
+				bbs.setBbsAvailable(rs.getInt(6));
+				return bbs;	
+			}			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null; 
+	}
 }
