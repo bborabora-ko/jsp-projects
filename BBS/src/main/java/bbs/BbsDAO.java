49bfc2190657e3d63a2d6b7d39b4b7c85f85cbc4
bborabora-ko <bborabora.ko@gmail.com>
2022-07-01 22:14:23 +0900

    update

--- BBS/src/main/java/bbs/BbsDAO.java
+++ BBS/src/main/java/bbs/BbsDAO.java
@@ -134,4 +134,23 @@ public class BbsDAO {
 		}
 		return null; 
 	}
+	
+	
+	public int update(Integer bbsID, String bbsTitle, String bbsContent) {
+		String SQL = "UPDATE BBS SET bbsTitle = ?, bbsContent =? , WHERE bbsID = ?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, bbsTitle);
+			pstmt.setString(2, bbsContent);
+			pstmt.setInt(3, bbsID);
+
+			return pstmt.executeUpdate(); //성공시 0이상의 결과 반환
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return -1;	
+	}
+	
+	
 }
